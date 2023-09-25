64ea6e08f73f647e2ed96854f9d57f2ef7059f2e
mayakey <18213346+mayakey@users.noreply.github.com>
2023-09-25 17:11:23 +0900

    Add ABC056.A accepted code.

---
+++ src/ABC056.A.cpp
@@ -0,0 +1,15 @@
+/**
+ * URL: https://atcoder.jp/contests/abc056/tasks/abc056_a
+ * BGN: 2023-09-25 16:58:23
+ * END: 
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    char a, b;
+    cin  >> a >> b;
+    cout << (a == b ? "H" : "D") << endl;
+    return 0;
+}
